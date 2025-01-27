03b37b76c0f1d302ab2647effffc47a31da48f0b
Pathan Rehan Shakil Khan <71642588+rehanp1@users.noreply.github.com>
2025-01-27 20:27:10 +0530

    Update QuestionList.js
    added numbering to questions

--- client/src/components/QuestionList.js
+++ client/src/components/QuestionList.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const QuestionList = ({ data = [] }) => {
+const QuestionList = ({ data = [], page, limit }) => {
   return (
     <div className="questions-container">
       {data.map(({ title, type }, idx) => (
         <div className="question-list" key={idx}>
           <h4>
-            {idx + 1} Question: {title}
+            {idx + 1 + (page - 1) * limit} Question: {title}
           </h4>
           <p>Type: {type}</p>
         </div>
